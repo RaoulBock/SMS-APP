59b17c0e2741ce0f9e8524c6892b766aca8b20c7
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-17 11:35:16 +0200

    front-en

--- Context/AppContext.js
+++ Context/AppContext.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { APP_PAGES } from "./settings";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 export const AppContext = React.createContext({
   navPage: "",
@@ -8,6 +10,28 @@ export const AppContext = React.createContext({
 
 const AppProvider = ({ children }) => {
   const [navPage, setNavPage] = React.useState(APP_PAGES.APP.HOME);
+
+  React.useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBaatm0aQmB6vb1-hayXeEWbBomnmtkp7U",
+      authDomain: "chat-app-f916a.firebaseapp.com",
+      databaseURL: "https://chat-app-f916a-default-rtdb.firebaseio.com",
+      projectId: "chat-app-f916a",
+      storageBucket: "chat-app-f916a.appspot.com",
+      messagingSenderId: "118007497214",
+      appId: "1:118007497214:web:cd9acb622be29469aec0af",
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    const storeHighScore = (userId, score) => {
+      const db = getDatabase();
+      const reference = ref(db, "users/" + userId);
+      set(reference, {
+        highscore: score,
+      });
+    };
+  }, []);
 
   return (
     <AppContext.Provider
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -6,6 +6,8 @@ import {
   StatusBar,
   Button,
   FlatList,
+  TouchableOpacity,
+  TextInput,
 } from "react-native";
 import React from "react";
 import Nav from "../Components/Nav/Nav";
@@ -13,18 +15,54 @@ import SearchInput from "../Components/Inputs/SearchInput";
 import * as SMS from "expo-sms";
 import { data } from "../Context/settings";
 import List from "../Components/List/List";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 const HomeScreen = () => {
+  const [userScore, setUserScore] = React.useState(10);
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaatm0aQmB6vb1-hayXeEWbBomnmtkp7U",
+    authDomain: "chat-app-f916a.firebaseapp.com",
+    databaseURL: "https://chat-app-f916a-default-rtdb.firebaseio.com",
+    projectId: "chat-app-f916a",
+    storageBucket: "chat-app-f916a.appspot.com",
+    messagingSenderId: "118007497214",
+    appId: "1:118007497214:web:cd9acb622be29469aec0af",
+  };
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
+
+  const storeHighScore = (userId, score) => {
+    const db = getDatabase();
+    const reference = ref(db, "users/" + userId);
+    set(reference, {
+      highscore: score,
+    });
+  };
+
   return (
     <View style={styles.outline}>
       <Nav title={"Chat App"} />
       <SearchInput placeholder={"Search"} multiline={false} />
       <View>
-        <FlatList
+        {/* <FlatList
           data={data}
           renderItem={({ item }) => <List item={item} />}
           keyExtractor={(item) => item.toString()}
+        /> */}
+
+        <TextInput
+          placeholder="Send your new high score"
+          style={styles.text}
+          onChangeText={(text) => setUserScore(text)}
         />
+
+        <TouchableOpacity
+          onPress={storeHighScore("user001223231", userScore)}
+          style={styles.btn}
+        >
+          <Text>Send data</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -36,5 +74,14 @@ const styles = StyleSheet.create({
   outline: {
     flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  btn: {
+    backgroundColor: "gray",
+    padding: 10,
+  },
+  text: {
+    width: 200,
+    backgroundColor: "red",
+    padding: 20,
   },
 });
